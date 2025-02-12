0b917783ad66e583f8f8f5ca510dd31c369080bd
unknown <lyntechi@gmail.com>
2025-02-12 18:25:42 -0500

    #31: added js fix to non multi-selectable components and updated the demo structure correctly

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -9,18 +9,31 @@
         accordionButtons.each(function () {
           let button = $(this);
           let itemId = button.attr("aria-controls");
-
+                  
           // Retrieve the last stored state from localStorage (if available)
           let savedState = localStorage.getItem(itemId);
           if (savedState !== null) {
             button.attr("aria-expanded", savedState);
           }
+        });
 
-          // Update localStorage when the accordion button is clicked
-          button.on("click", function () {
-            let newState = button.attr("aria-expanded") === "true" ? "false" : "true";
-            localStorage.setItem(itemId, newState);
-          });
+        // Update localStorage when the accordion button is clicked
+        accordionButtons.on("click", function () {
+          let button = $(this);
+          let itemId = button.attr("aria-controls");
+          let newState = button.attr("aria-expanded") === "true" ? "false" : "true";
+          localStorage.setItem(itemId, newState);
+
+          // If this is NOT a multiselectable accordion, update all other buttons
+          if (!accordion.is("[data-allow-multiple]")) {
+            accordionButtons.not(button).each(function () {
+              let otherButton = $(this);
+              let otherItemId = otherButton.attr("aria-controls");
+
+              // Since non multiselectable auto-closes other opened accordions, make sure they are marked as "false" in storage
+              localStorage.setItem(otherItemId, "false");
+            });
+          }
         });
       });
     },
